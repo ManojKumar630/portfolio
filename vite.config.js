df06b456b0fcbe2fedd4bc87952fcb1c7d28602c
ManojKumar630 <manojkumarkondaparthii@gmail.com>
2025-04-10 12:24:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   base: '/portfolio/', 
 });
